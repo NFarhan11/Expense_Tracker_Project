f0f25d9d517cdf152f36226a35742a0d5e984ed1
NFarhan11 <najmifarhan9903@gmail.com>
2025-05-03 16:30:42 +0800

    default layout with toggle & responsiveness

--- frontend/components/layouts/SideBar.vue
+++ frontend/components/layouts/SideBar.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="w-64 bg-white h-screen border-r flex flex-col">
+  <div
+    class="w-64 bg-white h-screen border-r flex flex-col fixed top-0 z-20 transition-transform duration-300"
+    :class="{
+      'translate-x-0': isOpen,
+      '-translate-x-full': !isOpen,
+      'left-0': !isMobile,
+      'left-0': isMobile,
+    }"
+  >
     <div class="flex items-center h-20 px-4 space-x-3">
       <UIcon name="mdi:bar-chart" class="w-8 h-8 text-indigo-600" />
-      <span class="text-2xl font-bold text-gray-800">TailAdmin</span>
+      <span class="text-2xl font-bold text-gray-800">ExpTracker</span>
     </div>
     <nav class="flex-1 px-4 space-y-4">
       <div>
@@ -42,4 +50,15 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  isOpen: {
+    type: Boolean,
+    default: true,
+  },
+  isMobile: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,17 +1,52 @@
 <template>
   <div class="min-h-screen flex bg-gray-100">
     <!-- Sidebar -->
-    <LayoutsSideBar />
+    <LayoutsSideBar :is-open="isSidebarOpen" />
 
     <!-- Main content -->
-    <div class="flex flex-col flex-1">
+    <div
+      class="flex flex-col flex-1"
+      :class="{
+        'ml-64': isSidebarOpen && !isMobile,
+        'ml-0': !isSidebarOpen || isMobile,
+      }"
+    >
       <!-- Topbar -->
-      <LayoutsTopBar />
+      <LayoutsTopBar
+        @toggle-sidebar="toggleSidebar"
+        :is-sidebar-open="isSidebarOpen"
+      />
 
       <!-- Dashboard Content -->
-      <slot />
+      <div class="pt-20">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const isSidebarOpen = ref(true);
+const isMobile = ref(false);
+
+// Toggle SideBar
+const toggleSidebar = () => {
+  console.log("window width: ", window.innerWidth);
+  isSidebarOpen.value = !isSidebarOpen.value;
+};
+
+// Mobile Responsiveness
+const checkMobile = () => {
+  isMobile.value = window.innerWidth < 768;
+  if (isMobile.value) {
+    isSidebarOpen.value = false; // Close sidebar by default on mobile
+  } else {
+    isSidebarOpen.value = true; // Open sidebar by default on desktop
+  }
+};
+
+onMounted(() => {
+  checkMobile();
+  console.log("app mounted");
+});
+</script>
