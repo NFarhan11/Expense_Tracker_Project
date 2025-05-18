60a55c194d4001aa96ac6758289d8f2b95322403
NFarhan11 <najmifarhan9903@gmail.com>
2025-05-18 15:55:20 +0800

    added logout button

--- frontend/components/layouts/TopBar.vue
+++ frontend/components/layouts/TopBar.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="bg-white dark:bg-dark-bg w-full p-5 fixed top-0 z-10 transition-transform duration-300" :class="{
-    'left-64': isSidebarOpen && !isMobile,
-    'left-0': !isSidebarOpen || isMobile,
+  <div class="bg-white dark:bg-dark-bg p-5 fixed top-0 z-10 transition-all duration-300" :class="{
+    'left-64 w-[calc(100%-16rem)]': isSidebarOpen && !isMobile,
+    'left-0 w-full': !isSidebarOpen || isMobile,
   }">
-    <UButton icon="mdi:hamburger-menu" color="primary" variant="soft" @click="$emit('toggle-sidebar')" />
-    <ColorModeButton />
+    <div class="flex items-center justify-between">
+      <!-- Left Controls -->
+      <div class="flex items-center space-x-2">
+        <UButton icon="mdi:hamburger-menu" color="primary" variant="soft" @click="$emit('toggle-sidebar')" />
+        <ColorModeButton />
+      </div>
+
+      <!-- Right Controls -->
+      <div>
+        <UButton label="Logout" @click="logout" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,4 +31,10 @@ defineProps({
 });
 
 defineEmits(["toggle-sidebar"]);
+
+// Logout Button
+const logout = () => {
+  localStorage.removeItem('token');
+  return navigateTo('/login');
+};
 </script>
